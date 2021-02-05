05b527a10d3f7503217bdee90f843c1962fd818f
ug <gr8.distance@gmail.com>
2021-02-05 16:46:54 +0900

    🍷 jason

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule ChatworkEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
+      {:jason, "~> 1.2"}
     ]
   end
 end
